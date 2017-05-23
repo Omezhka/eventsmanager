a2ba3ed3e0f6d0029a338a67a87ebc3ef02d332d
Averkova Natalya <cynopterussphinx@gmail.com>
2017-05-23 14:15:40 +0300

    add Event model

---
+++ models/Event.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "event".
+ *
+ * @property integer $id_event
+ * @property string $datestart_event
+ * @property string $datestop_event
+ * @property string $time_start
+ * @property string $time_stop
+ * @property string $what
+ */
+class Event extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'event';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['datestart_event', 'datestop_event', 'time_start', 'time_stop', 'what'], 'required'],
+            [['datestart_event', 'datestop_event', 'time_start', 'time_stop'], 'safe'],
+            [['what'], 'string', 'max' => 500],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id_event' => 'Id Event',
+            'datestart_event' => 'Datestart Event',
+            'datestop_event' => 'Datestop Event',
+            'time_start' => 'Time Start',
+            'time_stop' => 'Time Stop',
+            'what' => 'What',
+        ];
+    }
+}
