808843c845a4f6985b360cf37835c1c1555fe58a
eventsma <eventsma@cpanel4.d.fozzy.com>
2017-05-30 13:57:15 +0200

    delete

--- views/Event/_event.php
+++
@@ -1,7 +0,0 @@
-<?php
-use yii\helpers\Html;
-use yii\helpers\HtmlPurifier;
-?>
-<div class="post">
-    <p><?= Html::a(Html::encode($model->name), ['view', 'id' => $model->id_event]); ?></p> 
-</div>
--- views/Members/_members.php
+++
@@ -1,7 +0,0 @@
-<?php
-use yii\helpers\Html;
-use yii\helpers\HtmlPurifier;
-?>
-<div class="post">
-    <p><?= Html::a(Html::encode($model->firstname_rus . ' ' . $model->lastname_rus ), ['../members/view', 'id' => $model->id]); ?></p> 
-</div>
